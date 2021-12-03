81f127262b0a3415e017c1ac1ab98e665405b4e5
konradko <konrad@korzel.com>
2021-12-03 13:31:34 +0100

    Update test comments

--- tests/test_order.py
+++ tests/test_order.py
@@ -13,9 +13,7 @@ TRACKING_CODE_2 = "5678"
 
 @pytest.fixture
 def order():
-    return Order(
-        line_items={SKU_1: LineItemInfo(quantity=2), SKU_2: LineItemInfo(quantity=2)}
-    )
+    return Order(line_items={SKU_1: LineItemInfo(quantity=2), SKU_2: LineItemInfo(quantity=2)})
 
 
 @pytest.mark.parametrize(
@@ -23,11 +21,7 @@ def order():
     (
         # Single part and one tracking code
         (
-            [
-                ShipmentUpdate(
-                    tracking_code=TRACKING_CODE_1, skus=[SKU_1, SKU_1, SKU_2, SKU_2]
-                )
-            ],
+            [ShipmentUpdate(tracking_code=TRACKING_CODE_1, skus=[SKU_1, SKU_1, SKU_2, SKU_2])],
             {TRACKING_CODE_1},
         ),
         # Multipart and one tracking code
@@ -55,9 +49,7 @@ def order():
         "multipart-multiple_tracking_codes",
     ],
 )
-def test_order_add_shipments_success_all_shipped(
-    shipment_updates, expected_tracking_codes, order
-):
+def test_order_add_shipments_success_all_shipped(shipment_updates, expected_tracking_codes, order):
     OrderService.add_shipments(order, shipment_updates)
 
     for line_item_info in order.line_items.values():
@@ -70,11 +62,8 @@ def test_order_add_shipments_success_all_shipped(
 @pytest.mark.parametrize(
     "shipment_updates",
     (
-        [
-            ShipmentUpdate(
-                tracking_code=TRACKING_CODE_1, skus=[SKU_1, SKU_2, INVALID_SKU]
-            )
-        ],
+        # Single part
+        [ShipmentUpdate(tracking_code=TRACKING_CODE_1, skus=[SKU_1, SKU_2, INVALID_SKU])],
         [
             ShipmentUpdate(tracking_code=TRACKING_CODE_1, skus=[SKU_1, SKU_2]),
             ShipmentUpdate(tracking_code=TRACKING_CODE_1, skus=[INVALID_SKU]),
@@ -82,9 +71,7 @@ def test_order_add_shipments_success_all_shipped(
     ),
     ids=["single_part", "multipart"],
 )
-def test_order_add_shipments_fail_sku_not_present_in_line_items(
-    shipment_updates, order
-):
+def test_order_add_shipments_fail_sku_not_present_in_line_items(shipment_updates, order):
     with pytest.raises(ShipmentUpdateValidationError) as excinfo:
         OrderService.add_shipments(order, shipment_updates)
 
@@ -95,11 +82,9 @@ def test_order_add_shipments_fail_sku_not_present_in_line_items(
 @pytest.mark.parametrize(
     "shipment_updates",
     (
-        [
-            ShipmentUpdate(
-                tracking_code=TRACKING_CODE_1, skus=[SKU_1, SKU_1, SKU_2, SKU_2, SKU_2]
-            )
-        ],
+        # Single part
+        [ShipmentUpdate(tracking_code=TRACKING_CODE_1, skus=[SKU_1, SKU_1, SKU_2, SKU_2, SKU_2])],
+        # Multipart
         [
             ShipmentUpdate(tracking_code=TRACKING_CODE_1, skus=[SKU_1, SKU_1, SKU_2]),
             ShipmentUpdate(tracking_code=TRACKING_CODE_1, skus=[SKU_2, SKU_2]),
@@ -111,17 +96,16 @@ def test_order_add_shipments_fail_quantity_exceeded(shipment_updates, order):
     with pytest.raises(ShipmentUpdateValidationError) as excinfo:
         OrderService.add_shipments(order, shipment_updates)
 
-    assert (
-        f"Shipped quantity for SKU '{SKU_2}' exceeds order line item pending shipment quantity"
-        in str(excinfo)
-    )
+    assert f"Shipped quantity for SKU '{SKU_2}' exceeds order line item pending shipment quantity" in str(excinfo)
     assert shipment_updates not in order.accepted_shipment_updates
 
 
 @pytest.mark.parametrize(
     "shipment_updates",
     (
+        # Single part
         [ShipmentUpdate(tracking_code=TRACKING_CODE_1, skus=[SKU_1, SKU_1, SKU_2])],
+        # Multipart
         [
             ShipmentUpdate(tracking_code=TRACKING_CODE_1, skus=[SKU_1, SKU_2]),
             ShipmentUpdate(tracking_code=TRACKING_CODE_1, skus=[SKU_1]),
